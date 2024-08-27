145be4758e00b549f5096cc68d183a3cb82d63ba
Omneya Osman <omneyaAhm@banquemisr.com>
2024-08-27 17:43:09 +0300

    adding image

--- app/src/main/java/com/omneya/ci_cd_example/MainActivity.kt
+++ app/src/main/java/com/omneya/ci_cd_example/MainActivity.kt
@@ -5,13 +5,19 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
@@ -43,14 +49,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Hello $name!",
-        modifier = modifier.clickable {
-            Analytics.trackEvent("My custom event");
-//            Crashes.generateTestCrash()
-//            throw Exception("noo")
-        }
-    )
+    Column (verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()){
+        Text(
+            text = "Hello Hello $name!",
+            modifier = modifier.clickable {
+                Analytics.trackEvent("My custom event");
+                //            Crashes.generateTestCrash()
+                //            throw Exception("noo")
+            }
+        )
+        Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription ="Image" )
+    }
 }
 
 @Preview(showBackground = true)
